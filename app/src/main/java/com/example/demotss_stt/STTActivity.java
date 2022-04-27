f35184d446a705b752beaba6e64df8c369b7cb8e
Ly Van Huy <huylv@vitalify.asia>
2022-04-27 16:39:26 +0700

    demo STT

--- app/src/main/java/com/example/demotss_stt/STTActivity.java
+++ app/src/main/java/com/example/demotss_stt/STTActivity.java
@@ -1,14 +1,64 @@
 package com.example.demotss_stt;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Objects;
 
 public class STTActivity extends AppCompatActivity {
+
+    ImageButton btnSpeak;
+    EditText etContent;
+    private static final int REQUEST_CODE_SPEECH_INPUT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sttactivity);
+
+        btnSpeak = findViewById(R.id.btnSpeakSTT);
+        etContent = findViewById(R.id.etContentSaid);
+
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,
+                        Locale.getDefault());
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak to text");
+
+                try {
+                    startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT);
+                }
+                catch (Exception e) {
+                    Toast.makeText(STTActivity.this, " " + e.getMessage(), Toast.LENGTH_SHORT)
+                            .show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_SPEECH_INPUT) {
+            if (resultCode == RESULT_OK && data != null) {
+                ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                etContent.setText(Objects.requireNonNull(result).get(0));
+            }
+        }
     }
 }
